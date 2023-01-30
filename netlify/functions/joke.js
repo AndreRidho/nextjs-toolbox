0778cd9cd80e4b139241b242b66f963a827738c7
MarkSuckmyberg <mbridho@gmail.com>
2023-01-30 21:38:49 +0800

    edit to teh lamda ducntion

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -1,15 +1,51 @@
-// Jokes provided from the lovely folks at https://icanhazdadjoke.com
-import jokes from './jokes.json';
+const nodemailer = require("nodemailer");
 
-export const handler = async (event) => {
-    // Generates a random index based on the length of the jokes array
-    const randomIndex = Math.floor(Math.random() * jokes.length)
-    const randomJoke = jokes[randomIndex]
-    
-    // Netlify Functions need to return an object with a statusCode
-    // Other properties such as headers or body can also be included.
-    return {
-        statusCode: 200,
-        body: JSON.stringify(randomJoke)
+exports.handler = async (event, context, callback) => {
+  // Generate OTP
+  const otp = Math.floor(Math.random() * 1000000);
+
+  // Send OTP to user's email
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "wetmemeslayer1@gmail.com",
+      pass: "andreridho"
     }
-}
+  });
+
+  const mailOptions = {
+    from: "wetmemeslayer1@gmail.com",
+    to: event.queryStringParameters.email,
+    subject: "OTP for your transaction",
+    text: `Your OTP is: ${otp}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("OTP sent: " + info.response);
+    }
+  });
+
+  // Compare OTP
+  const receivedOtp = event.queryStringParameters.otp;
+  let result;
+  if (receivedOtp === otp) {
+    result = "Success";
+  } else {
+    result = "Fail";
+  }
+
+  // Return result
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      result: result,
+      otpExpiry: 300 // 5 minutes in seconds
+    })
+  };
+  callback(null, response);
+};
