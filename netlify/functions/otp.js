f400e0a942291fb64d18f16e7b431008091a065c
MarkSuckmyberg <mbridho@gmail.com>
2023-02-03 15:37:50 +0800

    dev

--- netlify/functions/otp.js
+++ netlify/functions/otp.js
@@ -44,8 +44,8 @@ async function sendOTP(email, otp) {
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD
+      user: "wetmemeslayer1@gmail.com",
+      pass: "rotltdskaidasezd"
     }
   });
 
