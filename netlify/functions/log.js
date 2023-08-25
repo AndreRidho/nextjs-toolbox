8c9e6d67a45ceaab8dc91814096ce6159338b38d
Andre <mbridho@gmail.com>
2023-08-25 18:29:44 +0800

    Message

--- netlify/functions/log.js
+++ netlify/functions/log.js
@@ -1,25 +1,14 @@
 exports.handler = async function (event, context, callback) {
-//   return {
-//     statusCode: 400,
-//     headers: {
-//       "Access-Control-Allow-Origin": "*",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       result: "Fail",
-//       message: "Invalid request",
-//     }),
-//   };
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      result: "Success",
-      message: "OTP is valid",
-    }),
-  };
+  if ("message" in body) {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: body.message,
+      }),
+    };
+  }
 };
