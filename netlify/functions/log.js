5c6d1098fcf59911a6a94f980cda122fd2427337
Andre <mbridho@gmail.com>
2023-08-25 18:52:51 +0800

    Bug fix

--- netlify/functions/log.js
+++ netlify/functions/log.js
@@ -3,9 +3,24 @@ const querystring = require("node:querystring");
 exports.handler = async function (event) {
   try {
     const queryParams = event.queryStringParameters;
-    
+
     if (queryParams && "message" in queryParams) {
-      console.log('message: ' + queryParams.message);
+      console.log("message: " + queryParams.message);
+
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: queryParams.message,
+        }),
+      };
+
+      console.log("Response:", response);
+
+      return response;
     } else {
       return {
         statusCode: 400,
@@ -20,7 +35,7 @@ exports.handler = async function (event) {
     }
   } catch (error) {
     console.error("Error:", error);
-    
+
     return {
       statusCode: 500,
       headers: {
