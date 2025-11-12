edd974930ce0ca378bf6ce35686b8db825ece79b
Muhammad Bimastirandra Ridho <mbimastirandra.ridho@corp.bm.co.id>
2025-11-12 17:04:11 +0700

    fix: outdated function

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
     console.log(new Date(), "Start ping");
     await fetch("https://hirfa-api.onrender.com/");
     console.log(new Date(), "End ping");
-    return res.status(200).send("Pinged!");
   } catch (error) {
     console.log(new Date(), "Error:", error);
   }
