d4371d60f323aae886defad7ca0c56344319b80b
Muhammad Bimastirandra Ridho <mbimastirandra.ridho@corp.bm.co.id>
2025-11-12 16:43:34 +0700

    feat: cron for ping

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,10 +1,14 @@
+import { schedule } from "@netlify/functions";
+
 export default async function handler(req, res) {
-  try {
-    console.log(new Date(), "Start ping");
-    await fetch("https://hirfa-api.onrender.com/");
-    console.log(new Date(), "End ping");
-    return res.status(200).send("Pinged!");
-  } catch (error) {
-    console.log(new Date(), "Error:", error);
-  }
+  schedule("*/10 * * * *", async () => {
+    try {
+      console.log(new Date(), "Start ping");
+      await fetch("https://hirfa-api.onrender.com/");
+      console.log(new Date(), "End ping");
+      return res.status(200).send("Pinged!");
+    } catch (error) {
+      console.log(new Date(), "Error:", error);
+    }
+  });
 }
